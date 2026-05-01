eff203094e262b1d418099a9ba3efbbb06e79b63
Gustavo Castanha <gustavocastanha44@users.noreply.github.com>
2026-05-01 20:17:30 -0300

    chore: optimize icon font loading

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <head>
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=swap" />
       </head>
       <body className={`${inter.variable} ${outfit.variable} antialiased`}>
         {children}
